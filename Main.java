a1970684b2d80cd9a36a57e5b41efd8214d113e3
Mjakot <hlebletjaslomaju@gmail.com>
2022-10-30 15:22:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
-
+public class Main {
+    public static void main(String[] args)
+    {
+//         System.out.println(Math.Summation(-2));
+//         System.out.prinlt((Math.Summation(2)));
+//         System.out.prinlt((Math.Summation(5)));
+      
+        
+      
+      
+    }
+}
