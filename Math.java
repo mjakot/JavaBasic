361ad1e58b1632ba32e539b39bddf297bb6ddc70
Mjakot <hlebletjaslomaju@gmail.com>
2022-10-30 15:18:39 +0200

    Update Math.java

--- Math.java
+++ Math.java
@@ -1,1 +1,22 @@
+public class Math
+{
+    public static int Summation(int num)
+    {
+        if (num < 0)
+        {
+            num *= num;
+        }
 
+        else if (num * num < 20)
+        {
+            num /= 2;
+        }
+
+        else
+        {
+            num *= -1;
+        }
+
+        return num;
+    }
+}
